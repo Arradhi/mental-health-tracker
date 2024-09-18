1d84ff512017ae461c9566d65a3492515f14878e
Muhammad Fadhlan Arradhi <fadhlanarradhi@gmail.com>
2024-09-18 14:04:43 +0700

    menambahkan login page

--- main/models.py
+++ main/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
+    mood = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
@@ -11,10 +15,3 @@ class MoodEntry(models.Model):
     def is_mood_strong(self):
         return self.mood_intensity > 5
     
-class MoodEntry(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
-
